6a1f77f014c18f5700c22dde84a90cbf7e65196b
Petrus <petrusheya@gmail.com>
2024-05-30 22:47:30 +0200

    Might be ok?? donno

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-import express from 'express'
+import express, { response } from 'express'
 import cors from 'cors'
 import puppeteer from 'puppeteer'
 import bodyParser from 'body-parser'
 import path from 'path'
-import fs from 'fs'
-import fileType from 'file-type'
+import fs, { createWriteStream } from 'fs'
+import {fileTypeFromBuffer} from 'file-type'
+import { fileURLToPath } from 'url'
+import imageDownloader from 'image-downloader'
+import download from 'image-downloader'
+import axios from 'axios'
 
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const PORT = 8080
 const app = express()
 app.listen(PORT, ()=> {
-    console.log('Server Up on port:', PORT)
+    console.log('Server up on port:', PORT)
 })
 app.use(
     cors({
         origin: '*'
     })
-)``
+)
 app.use(bodyParser.json({
     limit:'50mb'
 }))
 
-app.post('get-mockup', async (req,res)=>{
+app.post('/get-mockup', async (req,res)=>{
     let browser;
     let page;
-    let filename;
+    let filename = 'image.png'
     try {
         console.log('Received Request')
         const {url} = req.body
         console.log('Received Url', url)
         const modifiedUrl = modifyUrl(url)
         console.log('Modified Url', modifiedUrl)
-        
-        const base64Src = await downloadImage(modifiedUrl)
-        if(!base64Src){
-            throw new Error('Error downloading image')
-        }
-
-        const isFileCreated = await createFileFromBase64(base64Src)
-        if(isFileCreated.success){
-            filename = isFileCreated.path
-            console.log("Here's the path", isFileCreated.path)
-        }else{
-            throw new Error('Error Creating File')
-        }
+        await downloadImageToFile(modifiedUrl)
 
         browser = await puppeteer.launch({
             timeout: 120000,
             protocolTimeout: 600000,
-            headless: true,
+            headless: false,
         })
 
         page = await browser.newPage()
-        await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: downloadDir });
         page.setDefaultNavigationTimeout(900000)
         page.setDefaultTimeout(900000)
         await page.goto('https://printify.com/app/editor/77/99')
@@ -64,14 +58,13 @@ app.post('get-mockup', async (req,res)=>{
         console.log('Actual Upload Button Visible')
 
         //upload files
-        const [psdFileChooser] = await Promise.all([
+        const [designFileChooser] = await Promise.all([
             page.waitForFileChooser(),
             page.click('button[data-testid="addMyDeviceLayerButton"][data-analyticsid="addMyDeviceLayerButton"]')
         ])
-
-        //uploading design files
-        await psdFileChooser.accept([isDownloaded.filename])
+        await designFileChooser.accept(['image.png'])
         console.log('Uploading File...')
+
         await page.waitForSelector('button[data-testid="chipButton"].chip.body-text.inverted.selected.selectable[type="button"]')
         console.log('File Uploaded Successfully')       
         
@@ -79,6 +72,7 @@ app.post('get-mockup', async (req,res)=>{
         await page.keyboard.down('Control');
         await page.keyboard.press(']');
         await page.keyboard.up('Control');
+        console.log('Button Clicked')
 
         //preview section confirmation
         await page.waitForSelector('button[data-testid="button"].button.secondary.medium[type="button"]')
@@ -106,6 +100,23 @@ app.post('get-mockup', async (req,res)=>{
         return res.json({images: mockupImages})
     } catch (error) {
         console.error(error)
+        return res.status(500).send("Error occured, oopsie daisy")
+    }finally {
+        if (browser) {
+            await browser.close();
+        }
+        fs.unlink('./image.png', (err) => {
+            if (err) {
+              // Handle specific error if any
+              if (err.code === 'ENOENT') {
+                console.error('File does not exist.');
+              } else {
+                throw err;
+              }
+            } else {
+              console.log('File deleted!');
+            }
+        });
     }
 })
 
@@ -125,38 +136,35 @@ function modifyUrl(url){
 async function downloadImage(url){
     try {
       const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch image: ${response.statusText}`);
-      }
-      const buffer = await response.buffer();
-      const base64Image = buffer.toString('base64');
-      return base64Image;
+      if(!response){throw new Error('Error Fetching Image')}
+      const blob = await response.blob() 
 
-    } catch (error) {
+      const readerInstance = new FileReader()
+      readerInstance.readAsDataURL(blob)
+      readerInstance.onload = (e) =>{
+        console.log('the string is this long', e.target.result.length)
+        return e.target.result
+      }
+
+    } catch (error){
       console.error(`Error downloading image: ${error}`);
       return null;
     }
 }
-async function createFileFromBase64(base64String){
-    try {
-        // Decode the base64 string to binary data
-        const binaryData = Buffer.from(base64String, 'base64');
-        const type = await fileType.fileTypeFromBuffer(binaryData);
-    
-        // Use the detected file extension
-        const filename = `image.${type.ext? type.ext : 'png'}`;
-    
-        // Define the file path in the main directory
-        const filePath = path.join(__dirname, filename);
-      
-        // Write the binary data to a file
-        fs.writeFileSync(filePath, binaryData, 'binary');
-    
-        console.log(`File created at ${filePath}`);
-        return {success: true, path: filePath}
-        
-    } catch (error) {
-        console.error(error)
-        return ({success: false})
-    }
+async function downloadImageToFile(url){
+    const imagePath = 'image.png'
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    });
+
+    const writer = fs.createWriteStream(imagePath);
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
 }
